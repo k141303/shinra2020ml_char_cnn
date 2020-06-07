8514170f72bc58d9e0e1eca8ad52f0d83db9d9d6
kouta <k141303@edu.tut.ac.jp>
2020-06-07 23:10:58 +0900

    minor modification

--- script/run_predict.sh
+++ script/run_predict.sh
@@ -2,11 +2,9 @@ LANG=('ar' 'bg' 'ca' 'cs' 'da' 'de' 'el' 'en' 'es' 'fa' \
       'fi' 'fr' 'he' 'hi' 'hu' 'id' 'it' 'ko' 'nl' 'no' \
       'pl' 'pt' 'ro' 'ru' 'sv' 'th' 'tr' 'uk' 'vi' 'zh')
 
-TARGET="[Path to the directory containing the target data(e.g. minimum_en.zip or target.zip)]"
-
 for lang in ${LANG[@]}
 do
-python3 predict.py -i $TARGET \
+python3 predict.py -i $1 \
                    -o ./prediction \
                    --lang $lang \
                    --pretrained_model ./result
--- script/run_preprocess.sh
+++ script/run_preprocess.sh
@@ -1,3 +1,1 @@
-TRAINDATA="[Path to the directory containing the training data(e.g. trial_en.zip or minimum_en.zip)]"
-
-python3 preprocess.py -i $TRAINDATA
+python3 preprocess.py -i $1
